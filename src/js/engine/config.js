70a5b9d38f1c40aaaeeb22fc6737c6eb8b57351d
Matthias Neid <matthias.neid@ser.de>
2016-03-10 10:48:23 +0100

    asfda

---
+++ src/js/engine/config.js
@@ -0,0 +1,19 @@
+/**
+ * THREE.js Engine
+ * Config 
+ *
+ * written by Matthias Neid
+ **/
+
+var Config = {
+	FieldOfView:    75,
+	AspectRatio:    window.innerWidth/window.innerHeight,
+	PaneNear:       0.001,
+	PaneFar:        1000,
+	Width:          window.innerWidth,
+	Height:         window.innerHeight,
+	Size:           1,
+	Spacing:        0,
+	AntiAlias:      true,
+	Isometric:      true    
+}
